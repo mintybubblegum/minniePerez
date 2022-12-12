dca4504c535c8bf39919290a769c438aced6caff
Ana Rueda Guiu <ana_rg95@hotmail.com>
2022-12-12 13:53:01 +0100

    finished edit view

--- src/View/editChild.php
+++ src/View/editChild.php
@@ -12,19 +12,19 @@
             <form class="row g-3 cardBox" action="?action=store" method="post">
                 <div class="col-md-6">
                     <label class="form-label">Child Name*</label>
-                    <input type="text" class="form-control" name="childName" required>
+                    <input type="text" class="form-control" name="childName" required value='<?php echo $data["child"] -> getChildName() ?>'>
                 </div>
                 <div class="col-md-6">
                     <label for="inputPassword4" class="form-label">Age*</label>
-                    <input min="0" type="number" class="form-control" name="age" required>
+                    <input min="0" type="number" class="form-control" name="age" required value='<?php echo $data["child"] -> getAge() ?>'>
                 </div>
                 <div class="col-12">
                     <label for="inputAddress" class="form-label">Place*</label>
-                    <input type="text" class="form-control" name="place" placeholder="in the school playground" required>
+                    <input type="text" class="form-control" name="place" placeholder="in the school playground" required value='<?php echo $data["child"] -> getPlace() ?>'>
                 </div>
                 <div class="col-12">
                     <label class="form-label">Gift Suggestion</label>
-                    <input type="text" class="form-control" name="giftSuggestion">
+                    <input type="text" class="form-control" name="giftSuggestion" value='<?php echo $data["child"] -> getGiftSuggestion() ?>'>
                 </div>
                 <div class="buttons">
                     <button value="create" class="saveButton" style="background-image: url(./public/img/saveButton.png)"></button>
